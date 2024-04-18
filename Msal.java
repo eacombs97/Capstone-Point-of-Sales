391652b4769e9c0211ff88688cc5b8b63d4bd644
Skyler-Ireland <60803009+Skyler-Ireland@users.noreply.github.com>
2024-04-18 16:38:31 -0400

    Add files via upload

--- Msal.java
+++ Msal.java
@@ -2,20 +2,34 @@ import com.microsoft.aad.msal4j.*;
 import java.util.concurrent.*;
 import java.util.Collections;
 public class Msal {
-    ConfidentialClientApplication app = ConfidentialClientApplication.builder(
-                    "df9ad17d-483c-4fca-b70d-1e7df2c26e27",
-                    ClientCredentialFactory.createFromSecret("nIH8Q~XAyXynNDUOyxHqfX3fIGq-HIlWTkRVUcUY"))
-                    .authority("https://bitbuggy.b2clogin.com/tfp/bitbuggy.onmicrosoft.com/B2C_1_BitBuggyLogin")
-                    .build();
+    public static void main(String[] args) throws Exception {
+        // Define application properties
+        String clientId = "df9ad17d-483c-4fca-b70d-1e7df2c26e27";
+        String clientSecret = "nIH8Q~XAyXynNDUOyxHqfX3fIGq-HIlWTkRVUcUY";
+        String authority = "https://login.microsoftonline.com/533398db-accf-4cf9-bee1-6b65ce5aa8b9";
 
-    ClientCredentialParameters parameters =
-            ClientCredentialParameters.builder(Collections.singleton("openid profile email password offline_access"))
-                    .build();
+        // Create ConfidentialClientApplication instance
+        ConfidentialClientApplication app = ConfidentialClientApplication.builder(clientId, ClientCredentialFactory.createFromSecret(clientSecret))
+                .authority(authority)
+                .build();
 
-    CompletableFuture<IAuthenticationResult> future = app.acquireToken(parameters);
-    IAuthenticationResult result = future.get();
+        // Define authentication parameters
+        ClientCredentialParameters parameters = ClientCredentialParameters.builder(Collections.singleton("openid profile email password"))
+                .build();
 
-    String accessToken = result.accessToken();
+        try {
+            // Acquire token asynchronously
+            CompletableFuture<IAuthenticationResult> future = app.acquireToken(parameters);
 
-        System.out.println("Access token: " + accessToken);
+            // Wait for token acquisition to complete and handle response
+            IAuthenticationResult result = future.get();
+
+            // Extract access token
+            String accessToken = result.accessToken();
+            System.out.println("Access token: " + accessToken);
+        } catch (ExecutionException | InterruptedException e) {
+            // Handle exception
+            e.printStackTrace();
+        }
+    }
 }
