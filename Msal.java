ff619d9f1c6af9517bdffde90e9a591756c55fe1
Skyler-Ireland <60803009+Skyler-Ireland@users.noreply.github.com>
2024-04-18 15:30:49 -0400

    Add files via upload

--- Msal.java
+++ Msal.java
@@ -5,11 +5,11 @@ public class Msal {
     ConfidentialClientApplication app = ConfidentialClientApplication.builder(
                     "df9ad17d-483c-4fca-b70d-1e7df2c26e27",
                     ClientCredentialFactory.createFromSecret("nIH8Q~XAyXynNDUOyxHqfX3fIGq-HIlWTkRVUcUY"))
-                    .authority("https://login.microsoftonline.com/533398db-accf-4cf9-bee1-6b65ce5aa8b9")
+                    .authority("https://bitbuggy.b2clogin.com/tfp/bitbuggy.onmicrosoft.com/B2C_1_BitBuggyLogin")
                     .build();
 
     ClientCredentialParameters parameters =
-            ClientCredentialParameters.builder(Collections.singleton("openid profile email password"))
+            ClientCredentialParameters.builder(Collections.singleton("openid profile email password offline_access"))
                     .build();
 
     CompletableFuture<IAuthenticationResult> future = app.acquireToken(parameters);
